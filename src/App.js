fd147f397ea0f22c70460098b14cbd2b0c82a8f5
shreya-nipanikar <nipanikar.shreya1@gmail.com>
2021-05-24 12:51:32 -0400

    my todolist

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
+import { GoPlus } from 'react-icons/go';
+import Input from '@material-ui/core/Input';
+import InputAdornment from '@material-ui/core/InputAdornment';
+import DeleteOutlineIcon from '@material-ui/icons/DeleteOutline';
 
-function App() {
+
+const App = () => {
+
+   const [itemToAdd, setItemToAdd] = useState("");
+   const [addItem, setAddItem] = useState([]);
+   
+  const addItemFun = () => {
+    if(!itemToAdd){
+
+    }else {
+      setAddItem([...addItem,itemToAdd]);
+      setItemToAdd('');
+    }
+}
+
+const deleteFun = (indexx) => {
+  const upDatedValue = addItem.filter((element,index)=> {
+    return index !== indexx;
+  })
+  setAddItem(upDatedValue);
+}
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+    <div className="container">
+    <h1>ToDo-List</h1>
+
+    <Input type="text" className="myInput" placeholder="Enter Item" value={itemToAdd}
+    onChange={(e)=>setItemToAdd(e.target.value)}  endAdornment= { <InputAdornment position="end"><GoPlus className="goplus" onClick={addItemFun} /></InputAdornment> } ></Input>
+   
+
+     
+     <h1 className="Check">Your Check List:</h1>
+     
+     
+     {
+       addItem.map((result,index) => {
+         return (
+           <ul className="check_delete" key={index}>
+             <li>
+             <h3 >{result} <DeleteOutlineIcon className="deleteIcon" onClick={()=>deleteFun(index) } /></h3>
+             </li>
+           </ul>
+         )
+        })
+     }
+     
+    
     </div>
-  );
+    </div>
+  )
+
 }
 
 export default App;
